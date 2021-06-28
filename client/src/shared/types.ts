886da4ff121cd96aac80cfaecfcce437e343772a
Viktor <ricchiuto.viktor@gmail.com>
2021-06-28 17:52:02 +0200

    feat: add shared types

---
+++ client/src/shared/types.ts
@@ -0,0 +1,33 @@
+export type TechName = 'javascript' | 'angular';
+
+export type Level = 'junior' | 'senior' | 'tutor' | 'CEO';
+
+export interface Tech {
+  name: TechName;
+  level: Level;
+}
+
+export type Minigame = 'memory';
+
+export type Activity = Minigame | 'assessment';
+
+export type StarsCount = 0 | 1 | 2 | 3;
+
+export type TechExperience = Record<TechName, number>;
+
+export interface PastActivity {
+  name: Activity;
+  topic: TechName;
+  stars: StarsCount;
+}
+
+export interface UserGameData {
+  techExperience: TechExperience;
+  history: PastActivity[];
+  level: Level;
+}
+
+export interface User {
+  username: string;
+  gameData: UserGameData;
+}
