bafa7a9ed91bc676afa0fe8ee61f698322ecab22
davidspanjaard <dmspanjaard@gmail.com>
2021-06-29 18:03:33 +0200

    feat: future schedule component card added

--- client/src/components/FutureScheduleCard/FutureScheduleCard.tsx
+++ client/src/components/FutureScheduleCard/FutureScheduleCard.tsx
@@ -1,1 +1,16 @@
-export default {};
+import React from 'react';
+import TechIcon from '../TechIcon';
+import ScheduleItemHeader from '../ScheduleItemHeader';
+import styles from './FutureScheduleCard.module.scss';
+
+const FutureScheduleCard = (): JSX.Element => {
+  return (
+    <div className={styles.scheduleItem}>
+      <ScheduleItemHeader scheduleItemTopic="undefined" />
+      <TechIcon techName="javascript" iconSize="large" isGray isLocked />
+      <p>Who knows what the future will bring</p>
+    </div>
+  );
+};
+
+export default FutureScheduleCard;
