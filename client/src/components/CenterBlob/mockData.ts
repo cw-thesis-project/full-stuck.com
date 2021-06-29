eabfc1b1a2b0fdbf1fcf7d6b02dd927bcb96b2de
Viktor <ricchiuto.viktor@gmail.com>
2021-06-29 12:05:12 +0200

    feat: add center blob component for assessment

---
+++ client/src/components/CenterBlob/mockData.ts
@@ -0,0 +1,5 @@
+import { TechName } from '../../shared/types';
+
+export const oneTechName: TechName[] = ['javascript'];
+export const twoTechNames: TechName[] = ['javascript', 'git'];
+export const threeTechNames: TechName[] = ['javascript', 'git', 'react'];
