5657804b2548edcedcba3a3a7c9e1c2571a8a661
davidspanjaard <dmspanjaard@gmail.com>
2021-06-29 13:28:14 +0200

    feat: logic for failed and success past component

--- client/src/components/PastScheduleCard/PastScheduleCard.tsx
+++ client/src/components/PastScheduleCard/PastScheduleCard.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { StarsCount, TechName } from '../../shared/types';
+import TechIcon from '../TechIcon';
+import styles from './PastScheduleCard.module.scss';
 
 interface Props {
   isFailed: boolean;
+  stars: StarsCount;
+  topic: TechName;
 }
-const PastScheduleCard = ({ isFailed }: Props): JSX.Element => {
-  return <p>{`${isFailed}`}</p>;
+const PastScheduleCard = ({ isFailed, stars, topic }: Props): JSX.Element => {
+  return (
+    <>
+      {isFailed ? (
+        <div className={styles.scheduleItem}>
+          <p>{`${topic}`}</p>
+          <p>{`${stars}`}</p>
+          <TechIcon techName={topic} iconSize="large" isGray />
+          <p>nobody will ever hire you</p>
+          <p>Failed</p>
+        </div>
+      ) : (
+        <div className={styles.scheduleItem}>
+          <p>{`${topic}`}</p>
+          <p>{`${stars}`}</p>
+          <TechIcon techName={topic} iconSize="large" isGray={false} />
+          <p>Don’t you like it?</p>
+          <p>Learnt</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default PastScheduleCard;
