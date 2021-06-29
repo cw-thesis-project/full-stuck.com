6d3325ad77df482f259f8959b2644318866e0dba
Gil Reich <gilreich12@gmail.com>
2021-06-29 10:15:21 +0100

    feat: TechAchievementCard added

---
+++ client/src/components/LearntTech/TechAchievementCard/TechAchievementCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TechName } from '../../../shared/types';
+
+interface Props {
+  techName: TechName;
+  techSkillLevel: number;
+}
+
+const TechAchievementCard = ({
+  techName,
+  techSkillLevel,
+}: Props): JSX.Element => {};
+
+export default TechAchievementCard;
