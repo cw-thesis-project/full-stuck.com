a9fae48fe875c8472735d07e43d740641d3cca15
Gil Reich <gilreich12@gmail.com>
2021-06-29 20:00:54 +0100

    styling: WIP

--- client/src/containers/Dashboard/Dashboard.tsx
+++ client/src/containers/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Roadmap from '../../components/Roadmap';
 
 const Dashboard = () => {
-  return <Roadmap userLevel="tutor" />;
+  return <Roadmap userLevel="junior" />;
 };
 
 export default Dashboard;
