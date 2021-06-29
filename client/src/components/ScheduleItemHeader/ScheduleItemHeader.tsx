54ec528b3fc1b4899d4a6c0b0336cb72fe1a3297
davidspanjaard <dmspanjaard@gmail.com>
2021-06-29 15:41:10 +0200

    feat: SheduleItemHeader component
    Co-authored-by: Viktor Ricchiuto <ricchiuto.viktor@gmail.com>

---
+++ client/src/components/ScheduleItemHeader/ScheduleItemHeader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TechName } from '../../shared/types';
+import PastScheduleCard from '../PastScheduleCard/PastScheduleCard';
+import styles from './ScheduleItemHeader.module.scss';
+
+interface Props {
+  scheduleItemTopic: TechName;
+}
+
+const ScheduleItemHeader = ({ scheduleItemTopic }: Props) => {
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerColor} />
+      <h1>{scheduleItemTopic}</h1>
+    </div>
+  );
+};
+
+export default ScheduleItemHeader;
