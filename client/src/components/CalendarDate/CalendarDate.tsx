bdd692a12fb9d24879c5334448d9fa74467df51d
davidspanjaard <dmspanjaard@gmail.com>
2021-06-29 18:49:03 +0200

    feat: grid schedule layout

--- client/src/components/CalendarDate/CalendarDate.tsx
+++ client/src/components/CalendarDate/CalendarDate.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import styles from './CalendarDate.module.scss';
 
-type DateVariant = 'past' | 'current' | 'future';
+export type DateVariant = 'past' | 'current' | 'future';
 
 interface Props {
   variant: DateVariant;
--- client/src/containers/Schedule/Schedule.tsx
+++ client/src/containers/Schedule/Schedule.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Schedule from '../../components/Schedule';
-import { completedWeek } from '../../components/Schedule/mock';
+import { failedWeek } from '../../components/Schedule/mock';
 
 const ScheduleContainer = (): JSX.Element => {
   return (
     <div>
-      <Schedule history={completedWeek} />
+      <Schedule history={failedWeek} />
     </div>
   );
 };
