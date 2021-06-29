2632f9366e18121407cea8689c28b59f3ee36b37
Viktor <ricchiuto.viktor@gmail.com>
2021-06-29 11:14:55 +0200

    feat: add side column component for assessment

---
+++ client/src/components/SideColumn/SideColumn.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import { TechName } from '../../shared/types';
+import styles from './SideColumn.module.scss';
+import TechIcon from '../TechIcon';
+
+export interface IconDescriptor {
+  techName: TechName;
+  isGray: boolean;
+}
+
+interface Props {
+  icons: IconDescriptor[];
+  onIconMatch(index: number): void;
+}
+
+const SideColumn = ({ icons, onIconMatch }: Props): JSX.Element => {
+  return (
+    <div className={styles.sideColumn}>
+      {icons.map(({ isGray, techName }, index) => (
+        <div onClick={() => onIconMatch(index)}>
+          <TechIcon
+            isGray={isGray}
+            techName={techName}
+            iconSize="large"
+            key={index}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SideColumn;
---
+++ client/src/components/SideColumn/mockData.ts
@@ -0,0 +1,10 @@
+import { IconDescriptor } from './SideColumn';
+
+// eslint-disable-next-line import/prefer-default-export
+export const mockIcons: IconDescriptor[] = [
+  { isGray: true, techName: 'javascript' },
+  { isGray: false, techName: 'git' },
+  { isGray: true, techName: 'graphql' },
+  { isGray: true, techName: 'typescript' },
+  { isGray: false, techName: 'react' },
+];
