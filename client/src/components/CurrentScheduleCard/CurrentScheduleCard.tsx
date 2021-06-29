83a9ffe5456aec293fd09e85fd59f00fc4a58c90
davidspanjaard <dmspanjaard@gmail.com>
2021-06-29 18:05:11 +0200

    feat: CurrentSchedule component

--- client/src/components/CurrentScheduleCard/CurrentScheduleCard.tsx
+++ client/src/components/CurrentScheduleCard/CurrentScheduleCard.tsx
@@ -1,1 +1,16 @@
-export default {};
+import React from 'react';
+import ScheduleItemHeader from '../ScheduleItemHeader';
+import styles from './CurrentScheduleCard.module.scss';
+import icons from '../../assets/icons';
+
+const CurrentScheduleCard = (): JSX.Element => {
+  return (
+    <div className={styles.scheduleItem}>
+      <ScheduleItemHeader scheduleItemTopic="await" />
+      <img src={icons.hourGlassLogo} alt="hourGlass" />
+      <p>Who knows what the future will bring</p>
+    </div>
+  );
+};
+
+export default CurrentScheduleCard;
