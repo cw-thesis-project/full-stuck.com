de2c255688867040f2f1b4df562767b948eb8e0d
Gil Reich <gilreich12@gmail.com>
2021-06-29 09:50:49 +0100

    Feat: LearntTech component infrastructure

---
+++ client/src/components/Dashboard/LearntTech/LearntTech.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TechExperience } from '../../../shared/types';
+
+interface State {
+  firstTechIndex: number;
+}
+
+interface Props {
+  techExperience: TechExperience;
+}
+
+const LearntTech = ({ techExperience }: Props): JSX.Element => {
+  return;
+};
+
+export default LearntTech;
