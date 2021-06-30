42764c1a1cdde0f6318821f723cf34a11ee272b1
Viktor <ricchiuto.viktor@gmail.com>
2021-06-30 11:15:37 +0200

    feat: show every component in assessment screen

--- client/src/containers/Assessment/Assessment.tsx
+++ client/src/containers/Assessment/Assessment.tsx
@@ -1,7 +1,29 @@
+/* eslint-disable no-console */
 import React from 'react';
+import SideColumn, { mockIcons } from '../../components/SideColumn';
+import CenterBlob, { threeTechNames } from '../../components/CenterBlob';
+import CountDownBar from '../../components/CountDownBar';
+import styles from './Assessment.module.scss';
 
 const Assessment = (): JSX.Element => {
-  return <div>assesment</div>;
+  return (
+    <div className={styles.page}>
+      <SideColumn
+        icons={mockIcons}
+        onIconMatch={(index) => console.log('matched index', index)}
+      />
+      <div className={styles.centerSection}>
+        <h1>7.85s</h1>
+        <CenterBlob techNames={threeTechNames} />
+        <h2>0/15</h2>
+        <CountDownBar currentPercentage={0.7} />
+      </div>
+      <SideColumn
+        icons={mockIcons}
+        onIconMatch={(index) => console.log('matched index', index)}
+      />
+    </div>
+  );
 };
 
 export default Assessment;
