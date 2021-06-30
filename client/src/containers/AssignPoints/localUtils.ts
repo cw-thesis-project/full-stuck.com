a59acd695ff2af06516abc3646a4f9e7b08a1980
Calimeron <ron.chamma@gmail.com>
2021-06-30 17:11:03 +0200

    chore: local variables to be used

---
+++ client/src/containers/AssignPoints/localUtils.ts
@@ -0,0 +1,25 @@
+import { Tech } from '../../shared/types';
+
+export const technologies: Tech[] = [
+  { name: 'javascript', level: 'junior' },
+  { name: 'git', level: 'junior' },
+  { name: 'react', level: 'junior' },
+  { name: 'graphql', level: 'senior' },
+  { name: 'rxjs', level: 'senior' },
+  { name: 'typescript', level: 'senior' },
+  { name: 'debugging', level: 'tutor' },
+  { name: 'eloquence', level: 'tutor' },
+  { name: 'espionage', level: 'tutor' },
+];
+
+export const initialTechExperience = {
+  javascript: 0,
+  git: 0,
+  react: 0,
+  graphql: 0,
+  rxjs: 0,
+  typescript: 0,
+  debugging: 0,
+  eloquence: 0,
+  espionage: 0,
+};
