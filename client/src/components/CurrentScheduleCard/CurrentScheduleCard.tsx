dd6df17c3bd535b767b01e3220157cf8452af8df
Viktor <ricchiuto.viktor@gmail.com>
2021-07-01 22:47:19 +0200

    feat: add back icon to schedule
    allows to go back to the dashboard

--- client/src/components/CurrentScheduleCard/CurrentScheduleCard.tsx
+++ client/src/components/CurrentScheduleCard/CurrentScheduleCard.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ScheduleItemHeader from '../ScheduleItemHeader';
 import styles from './CurrentScheduleCard.module.scss';
 import icons from '../../assets/icons';
+import back from '../../assets/icons/back.svg';
 
 const CurrentScheduleCard = (): JSX.Element => {
   return (
-    <div className={styles.scheduleItem}>
-      <ScheduleItemHeader scheduleItemTopic="await" />
-      <div className={styles.imageContainer}>
-        <img src={icons.hourGlassLogo} alt="hourGlass" />
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <ScheduleItemHeader scheduleItemTopic="await" />
+        <div className={styles.imageContainer}>
+          <img src={icons.hourGlassLogo} alt="hourGlass" />
+        </div>
+        <p className={styles.quote}>Who knows what the future will bring</p>
       </div>
-      <p className={styles.quote}>Who knows what the future will bring</p>
+      <Link to="/dashboard" className={styles.backIcon}>
+        <img src={back} alt="back icon" />
+      </Link>
     </div>
   );
 };
