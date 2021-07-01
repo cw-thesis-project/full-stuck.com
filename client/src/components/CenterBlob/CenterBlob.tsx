f4858ae04de9bef0c741954d2703fab323e3b43e
davidspanjaard <dmspanjaard@gmail.com>
2021-07-01 12:43:14 +0200

    feat: added drag capability

--- client/src/components/CenterBlob/CenterBlob.tsx
+++ client/src/components/CenterBlob/CenterBlob.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { TechName } from '../../shared/types';
 import styles from './CenterBlob.module.scss';
 import icons from '../../assets/icons';
@@ -10,6 +10,35 @@ interface Props {
 }
 
 const CenterBlob = ({ techNames }: Props): JSX.Element => {
+  const [dragging, setDragging] = useState(false);
+
+  const dragItem = useRef();
+  const dragItemNode = useRef();
+
+  const handletDragStart = (
+    e: React.DragEvent<HTMLDivElement>,
+    techName: TechName
+  ) => {
+    console.log('Starting to drag', techName);
+
+    // dragItemNode.current = e.target;
+    // dragItemNode.current.addEventListener('dragend', handleDragEnd);
+    // dragItem.current = item;
+
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+  const handleDragEnd = (
+    e: React.DragEvent<HTMLDivElement> | null,
+    techName: TechName
+  ) => {
+    console.log('Ending drag', techName);
+    setDragging(false);
+    // dragItem.current = null;
+    // dragItemNode.current.removeEventListener('dragend', handleDragEnd);
+    // dragItemNode.current = null;
+  };
   const iconsContainerClass = assignIconsContainerClass(techNames.length);
 
   return (
@@ -17,12 +46,18 @@ const CenterBlob = ({ techNames }: Props): JSX.Element => {
       <img className={styles.blob} src={icons.blob} alt="blob" />
       <div className={iconsContainerClass}>
         {techNames.map((techName) => (
-          <TechIcon
-            iconSize="medium"
-            isGray={false}
-            techName={techName}
-            key={techName}
-          />
+          <div
+            draggable
+            onDragStart={(e) => handletDragStart(e, techName)}
+            onDragEnd={dragging ? (e) => handleDragEnd(e, techName) : null}
+          >
+            <TechIcon
+              iconSize="medium"
+              isGray={false}
+              techName={techName}
+              key={techName}
+            />
+          </div>
         ))}
       </div>
     </div>
