410041449b50e73759ce9e9940d00ee15274dca7
Viktor <ricchiuto.viktor@gmail.com>
2021-07-01 08:45:17 +0200

    feat: add wave svg assets

---
+++ client/src/components/ScreenBackground/waves/index.ts
@@ -0,0 +1,6 @@
+export { default as waveBottom } from './wave-bottom.svg';
+export { default as waveRight } from './wave-right.svg';
+export { default as waveLeft } from './wave-left.svg';
+export { default as waveTopLeft } from './wave-top-left.svg';
+export { default as waveBottomRight } from './wave-bottom-right.svg';
+export { default as waveDiagonal } from './wave-diagonal.svg';
