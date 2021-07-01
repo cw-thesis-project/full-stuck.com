ea3c30691e2f81b7ea5d78cb4451de41c44a5fd9
Pablo “Carbonell <pablocarbonell@icloud.com>
2021-07-01 16:46:38 +0200

    feat: components structure and logic

--- client/src/containers/MemoryGame/MemoryGame.tsx
+++ client/src/containers/MemoryGame/MemoryGame.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import styles from './MemoryGame.module.scss';
+import FlipsCounter from '../../components/FlipsCounter';
+import MatchedPile from '../../components/MatchedPile';
+import CardsTable from '../../components/CardsTable';
+
 const MemoryGame = (): JSX.Element => {
-  return <div>memory game</div>;
+  return (
+    <div className={styles.container}>
+      <FlipsCounter />
+      <CardsTable />
+      <MatchedPile />
+    </div>
+  );
 };
 
 export default MemoryGame;
