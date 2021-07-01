d50d4d6ab79f213ad79b82bbaa1f5799b489fa01
Calimeron <ron.chamma@gmail.com>
2021-07-01 17:18:09 +0200

    chore: Temp Test Zone component

---
+++ client/src/containers/AssignPoints/TempTestZone/TempTestZone.tsx
@@ -0,0 +1,82 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { FakeState } from '../localUtils';
+import { TechName } from '../../../shared/types';
+
+interface Props {
+  fakeAppState: FakeState;
+  setFakeAppState: Dispatch<SetStateAction<FakeState>>;
+}
+
+const TempTestZone = ({
+  fakeAppState,
+  setFakeAppState,
+}: Props): JSX.Element => {
+  const { techExperience } = fakeAppState.user.gameData;
+  const state: any = fakeAppState;
+
+  const techXPs = Object.entries(techExperience).map((singleTech) => {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            console.log(
+              'clicked',
+              state.user.gameData.techExperience[singleTech[0]]
+            );
+            state.user.gameData.techExperience[singleTech[0]] -= 1;
+            setFakeAppState(state);
+          }}
+          type="button"
+        >
+          -
+        </button>{' '}
+        {singleTech[0]} {singleTech[1]}
+        <button
+          type="button"
+          onClick={() => {
+            state.user.gameData.techExperience[singleTech[0]] += 1;
+            setFakeAppState(state);
+          }}
+        >
+          {' '}
+          +{' '}
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <button type="button"> - </button> Level:{' '}
+        {fakeAppState.user.gameData.level} <button type="button"> + </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            state.pointsToAssign -= 1;
+            setFakeAppState(state);
+          }}
+        >
+          {' '}
+          -{' '}
+        </button>{' '}
+        points to Assign {fakeAppState.pointsToAssign}{' '}
+        <button
+          type="button"
+          onClick={() => {
+            state.pointsToAssign += 1;
+            setFakeAppState(state);
+          }}
+        >
+          {' '}
+          +{' '}
+        </button>
+      </div>
+      {techXPs}
+    </div>
+  );
+};
+
+export default TempTestZone;
