574986a1103bef9eb2de45561550b7b3a1b04797
Calimeron <ron.chamma@gmail.com>
2021-07-01 17:17:58 +0200

    refactor: add assign cards to helper

--- client/src/containers/AssignPoints/helpers.tsx
+++ client/src/containers/AssignPoints/helpers.tsx
@@ -4,6 +4,7 @@ import { Level, TechExperience, TechName } from '../../shared/types';
 import CurrentLevelCard from '../../components/CurrentLevelCard';
 import CompletedLevelCard from '../../components/CompletedLevelCard';
 import NextLevelCard from '../../components/NextLevelCard';
+import { levelToNumber } from '../../utils/utils';
 
 interface TechExperienceSubset {
   name: TechName;
@@ -46,4 +47,49 @@ export function renderCompletedCard(lvl: Level): JSX.Element {
 }
 export function renderNextCard(lvl: Level): JSX.Element {
   return <NextLevelCard level={lvl} />;
+}
+
+export function assignCards(
+  level: Level,
+  showAll: boolean,
+  setLeftCard: React.Dispatch<React.SetStateAction<JSX.Element>>,
+  setMiddleCard: React.Dispatch<React.SetStateAction<JSX.Element>>,
+  setRightCard: React.Dispatch<React.SetStateAction<JSX.Element>>,
+  pointsToAssign: number,
+  techExperience: TechExperience,
+  onIconClick: (techName: TechName) => void
+): void {
+  const stage = levelToNumber[level];
+  if (showAll) {
+    setLeftCard(
+      renderCurrentCard('junior', pointsToAssign, techExperience, onIconClick)
+    );
+    setMiddleCard(
+      renderCurrentCard('senior', pointsToAssign, techExperience, onIconClick)
+    );
+    setRightCard(
+      renderCurrentCard('tutor', pointsToAssign, techExperience, onIconClick)
+    );
+    return;
+  }
+  setLeftCard(
+    stage > 0
+      ? renderCompletedCard('junior')
+      : renderCurrentCard('junior', pointsToAssign, techExperience, onIconClick)
+  );
+  if (stage === 0) {
+    setMiddleCard(renderNextCard('senior'));
+    setRightCard(renderNextCard('tutor'));
+  }
+  if (stage === 1) {
+    setMiddleCard(
+      renderCurrentCard('senior', pointsToAssign, techExperience, onIconClick)
+    );
+    setRightCard(renderNextCard('tutor'));
+  } else if (stage === 2) {
+    setMiddleCard(renderCompletedCard('senior'));
+    setRightCard(
+      renderCurrentCard('tutor', pointsToAssign, techExperience, onIconClick)
+    );
+  }
 }
