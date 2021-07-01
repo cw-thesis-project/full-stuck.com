97ce91ab4a60b9f35223c319586914f4f33f1fd2
Viktor <ricchiuto.viktor@gmail.com>
2021-07-01 18:22:24 +0200

    feat: handle game over after memory game

--- client/src/containers/MemoryGame/MemoryGame.tsx
+++ client/src/containers/MemoryGame/MemoryGame.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import styles from './MemoryGame.module.scss';
 import FlipsCounter from '../../components/FlipsCounter';
 import MatchedPile from '../../components/MatchedPile';
 import CardsTable from '../../components/CardsTable';
 import useMemoryGame from './useMemoryGame';
+import { actions, useAppDispatch, useAppSelector } from '../../store';
 
 const MemoryGame = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+  const loading = useAppSelector((state) => state.loading);
   const {
     lastMatched,
     matchesDone,
@@ -15,6 +20,31 @@ const MemoryGame = (): JSX.Element => {
     cards,
     handleCardChoice,
   } = useMemoryGame();
+
+  useEffect(() => {
+    const isGameOver = matchesDone >= cards.length / 2;
+    if (isGameOver) {
+      console.log('game over');
+      // save activity (async)
+      dispatch(
+        actions.saveActivity({
+          name: 'memory',
+          topic: 'git',
+          stars: 3,
+        })
+      );
+    }
+  }, [matchesDone]);
+
+  // after activity has been saved
+  useEffect(() => {
+    const isGameOver = matchesDone >= cards.length / 2;
+    if (isGameOver && !loading) {
+      console.log('activity saved');
+      dispatch(actions.setPointsToAssign(1));
+      history.replace('/assign-points');
+    }
+  }, [loading]);
 
   return (
     <div className={styles.container}>
