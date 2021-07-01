f114ad046af8dc3e995034326489962e6186460f
Calimeron <ron.chamma@gmail.com>
2021-07-01 10:25:31 +0200

    chore: merge mockUser.tsx & localUtils

--- client/src/containers/AssignPoints/localUtils.ts
+++ client/src/containers/AssignPoints/localUtils.ts
@@ -1,4 +1,4 @@
-import { Tech } from '../../shared/types';
+import { Tech, User } from '../../shared/types';
 
 export const technologies: Tech[] = [
   { name: 'javascript', level: 'junior' },
@@ -22,4 +22,50 @@ export const initialTechExperience = {
   debugging: 0,
   eloquence: 0,
   espionage: 0,
+};
+
+export interface FakeState {
+  pointsToAssign: number;
+  token: string;
+  user: User;
+  loading: boolean;
+  error: string;
+}
+
+const mockUser: User = {
+  username: 'Pablo',
+  gameData: {
+    techExperience: {
+      javascript: 3,
+      git: 2,
+      react: 0,
+      graphql: 0,
+      rxjs: 0,
+      typescript: 0,
+      debugging: 0,
+      eloquence: 0,
+      espionage: 0,
+    },
+    history: [
+      {
+        name: 'memory',
+        topic: 'javascript',
+        stars: 2,
+      },
+      {
+        name: 'assessment',
+        topic: 'javascript',
+        stars: 0,
+      },
+    ],
+    level: 'junior',
+  },
+};
+
+export const fakeState: FakeState = {
+  pointsToAssign: 2,
+  token: '',
+  user: mockUser,
+  loading: false,
+  error: '',
 };
