195e194970c8b644e32282de2af914a9ae2b0bbc
Gil Reich <gilreich12@gmail.com>
2021-07-01 09:51:00 +0100

    feat: Tech scrolling behaviour

--- client/src/components/LearntTech/LearntTech.tsx
+++ client/src/components/LearntTech/LearntTech.tsx
@@ -3,9 +3,6 @@ import { TechExperience, TechName } from '../../shared/types';
 import TechAchievementCard from './TechAchievementCard';
 import styles from './LearntTech.module.scss';
 
-interface State {
-  firstTechIndex: number;
-}
 export type TechAchievement = { level: number; isLocked: boolean };
 
 export type TechAchievements = Record<TechName, TechAchievement>;
@@ -15,23 +12,39 @@ interface Props {
 }
 
 const LearntTech = ({ techAchievements }: Props): JSX.Element => {
-  // const [firstTechIndex, setFirstTechIndex] = useState({});
+  const [startIndex, setStartIndex] = useState(0);
 
   const techKeys = Object.keys(techAchievements) as Array<keyof TechExperience>;
 
-  const achievementCards = techKeys.map((techName: TechName, i) => {
-    if (i > 4) return null;
-    return (
+  const achievementCards = techKeys
+    .slice(startIndex, startIndex + 5)
+    .map((techName: TechName) => (
       <TechAchievementCard
         techName={techName}
         key={techName}
         isLocked={techAchievements[techName].isLocked}
         techSkillLevel={techAchievements[techName].level}
       />
-    );
-  });
+    ));
 
-  return <div className={`${styles.row}`}>{achievementCards}</div>;
+  const showForwardButton = startIndex <= 3;
+  const showBackButton = startIndex > 0;
+
+  return (
+    <div>
+      {showBackButton && (
+        <button onClick={() => setStartIndex(startIndex - 1)} type="button">
+          {'<'}
+        </button>
+      )}
+      <div className={styles.row}>{achievementCards}</div>
+      {showForwardButton && (
+        <button onClick={() => setStartIndex(startIndex + 1)} type="button">
+          {'>'}
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default LearntTech;
