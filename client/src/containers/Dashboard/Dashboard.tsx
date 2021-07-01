027a9e0479f8bf5d45a6ffd0f76afc8bbd9b6e70
Gil Reich <gilreich12@gmail.com>
2021-07-01 17:41:27 +0100

    feat: dashboard styling files

--- client/src/containers/Dashboard/Dashboard.tsx
+++ client/src/containers/Dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import { useAppDispatch, useAppSelector } from '../../store';
 import LearntTech from '../../components/LearntTech/index';
 import Roadmap from '../../components/Roadmap';
 import { createTechAchievements, createGreeting } from './helpers';
+import styles from './Dashboard.module.scss';
 
 const Dashboard = (): JSX.Element => {
   const { user, isLoading, getAccessTokenSilently } = useAuth0();
@@ -46,7 +47,9 @@ const Dashboard = (): JSX.Element => {
         <div>Schedule</div>
       </Link>
       <LearntTech techAchievements={userTechAchievements} />
-      <Roadmap userLevel={userStore.gameData.level} />
+      <div className={styles.roadmap}>
+        <Roadmap userLevel={userStore.gameData.level} />
+      </div>
     </div>
   );
 };
