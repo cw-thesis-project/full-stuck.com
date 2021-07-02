5614c2f742923e1bebb189090f71aa595cdbd3ff
Pablo “Carbonell <pablocarbonell@icloud.com>
2021-07-02 13:12:41 +0200

    feat: implement deployment to GH Pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import * as dotenv from 'dotenv';
 import { Provider } from 'react-redux';
