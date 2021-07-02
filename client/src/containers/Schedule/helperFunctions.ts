2f92b5c0563de6c094dc92ed777bc4fa27876d85
Pablo Carbonell <pablocarbonell@icloud.com>
2021-07-02 16:58:08 +0200

    no unused variables

--- client/src/containers/Schedule/helperFunctions.ts
+++ client/src/containers/Schedule/helperFunctions.ts
@@ -6,19 +6,19 @@ export function lastFiveElements<T>(list: T[]): T[] {
   return reversed.slice(0, endIndex);
 }
 
-const technologies: Tech[] = [
-  { name: 'javascript', level: 'junior' },
-  { name: 'git', level: 'junior' },
-  { name: 'react', level: 'junior' },
-  { name: 'graphql', level: 'senior' },
-  { name: 'rxjs', level: 'senior' },
-  { name: 'typescript', level: 'senior' },
-  { name: 'debugging', level: 'tutor' },
-  { name: 'eloquence', level: 'tutor' },
-  { name: 'espionage', level: 'tutor' },
-];
+// const technologies: Tech[] = [
+//   { name: 'javascript', level: 'junior' },
+//   { name: 'git', level: 'junior' },
+//   { name: 'react', level: 'junior' },
+//   { name: 'graphql', level: 'senior' },
+//   { name: 'rxjs', level: 'senior' },
+//   { name: 'typescript', level: 'senior' },
+//   { name: 'debugging', level: 'tutor' },
+//   { name: 'eloquence', level: 'tutor' },
+//   { name: 'espionage', level: 'tutor' },
+// ];
 
-export function getNextActivity(techExperience: TechExperience): Activity {
+export function getNextActivity(): Activity {
   return Math.random() > 0.5 ? 'assessment' : 'memory';
   // for (const { name } of technologies) {
   //   if (techExperience[name] === 3) {
