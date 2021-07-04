e4719783a228bbbe3b1e9673accefb072e777752
Viktor <ricchiuto.viktor@gmail.com>
2021-07-04 15:50:19 +0200

    refactor: extract stars row component

---
+++ client/src/components/StarsRow/StarsRow.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './StarsRow.module.scss';
+import icons from '../../assets/icons';
+import { StarsCount } from '../../shared/types';
+
+const starsArray = [0, 1, 2];
+
+interface Props {
+  starsCount: StarsCount;
+}
+
+const StarsRow = ({ starsCount }: Props): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      {starsArray.map((number) => {
+        const className = classnames({
+          [styles.star]: true,
+          [styles.gray]: starsCount <= number,
+        });
+        return <img alt="star icon" className={className} src={icons.star} />;
+      })}
+    </div>
+  );
+};
+
+export default StarsRow;
