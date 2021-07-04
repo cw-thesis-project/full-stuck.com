075bcaf91426c7b297f1a5983d76ad9ce8ce3e80
Viktor <ricchiuto.viktor@gmail.com>
2021-07-04 15:08:19 +0200

    fix: assessment history
    now correctly shows the last 5 activities in order

--- client/src/containers/Schedule/helperFunctions.ts
+++ client/src/containers/Schedule/helperFunctions.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-restricted-syntax */
 import { maxTechnologyExperience, technologies } from '../../shared/constants';
 import { Activity, TechExperience } from '../../shared/types';
+import { deepCopy } from '../../shared/utils';
 
 export function lastFiveElements<T>(list: T[]): T[] {
-  const reversed = [...list].reverse();
-  const endIndex = list.length % 6;
-  return reversed.slice(0, endIndex);
+  const listCopy = deepCopy(list);
+  return listCopy.slice(list.length - 5, list.length);
 }
 
 export function getNextActivity(techExperience: TechExperience): Activity {
