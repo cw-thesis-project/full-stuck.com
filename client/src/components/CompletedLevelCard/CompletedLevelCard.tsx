1e7c546a1a6181b09a897643d675f04437f259de
Calimeron <ron.chamma@gmail.com>
2021-07-05 12:47:52 +0300

    feat: click on a completed card gives details
    Co-authored-by: Gil Reich <gilreich12@gmail.com>

--- client/src/components/CompletedLevelCard/CompletedLevelCard.tsx
+++ client/src/components/CompletedLevelCard/CompletedLevelCard.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Level } from '../../shared/types';
 import styles from './CompletedLevelCard.module.scss';
 import icons from '../../assets/icons';
+import TechMedal from './TechMedal';
+import { technologies } from '../../shared/constants';
 
 interface Props {
   level: Level;
 }
 
 const CompletedLevelCard = ({ level }: Props): JSX.Element => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const techMedals = technologies
+    .filter((obj) => {
+      return obj.level === level;
+    })
+    .map((tech) => <TechMedal techName={tech.name} />);
+
   return (
-    <div className={styles.container}>
+    <div
+      role="button"
+      tabIndex={0}
+      onKeyDown={() => console.log('')}
+      onClick={() => setShowDetails(!showDetails)}
+      className={styles.container}
+    >
       <div className={styles.title}>
         <h2>{level}</h2>
       </div>
-      <div className={styles.medalZone}>
-        <img src={icons.medal} alt="medal" className={styles.img} />
-        <h3>Completed</h3>
-      </div>
+      {showDetails ? (
+        techMedals
+      ) : (
+        <div className={styles.medalZone}>
+          <img src={icons.medal} alt="medal" className={styles.img} />
+          <h3>Completed</h3>
+        </div>
+      )}
       <div className={styles.bottom}>
         <h4>Be proud of yourself! This was not easy</h4>
       </div>
