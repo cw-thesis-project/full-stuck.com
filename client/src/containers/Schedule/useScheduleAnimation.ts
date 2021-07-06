2b0f189e26406213d6c0bfdc309b153e3b406500
Viktor <ricchiuto.viktor@gmail.com>
2021-07-06 07:42:28 +0200

    feat: finalize schedule page style

--- client/src/containers/Schedule/useScheduleAnimation.ts
+++ client/src/containers/Schedule/useScheduleAnimation.ts
@@ -3,10 +3,12 @@ import { useEffect } from 'react';
 import { animateFrom, fadeIn, slideRight } from '../../shared/animations';
 import calendarDate from '../../components/CalendarDate/CalendarDate.module.scss';
 import schedule from '../../components/Schedule/Schedule.module.scss';
+import container from './Schedule.module.scss';
 
 function useScheduleAnimation(): void {
   useEffect(animateScheduleCards, []);
   useEffect(animateCalendarDates, []);
+  useEffect(animateFooter, []);
 
   function animateScheduleCards() {
     animateFrom(schedule.scheduleCard, {
@@ -19,6 +21,15 @@ function useScheduleAnimation(): void {
     animateFrom(calendarDate.container, {
       ...fadeIn,
       stagger: 0.15,
+    });
+  }
+
+  function animateFooter() {
+    animateFrom(container.footer, {
+      y: '-2em',
+      opacity: 0,
+      ease: 'power3.out',
+      delay: 1,
     });
   }
 }
