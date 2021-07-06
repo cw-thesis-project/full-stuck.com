a0e75b8459ed1f3d89b53ed25e195f457d23a081
Viktor <ricchiuto.viktor@gmail.com>
2021-07-06 16:41:21 +0200

    feat: add game over screen in memory game

--- client/src/containers/MemoryGame/MemoryGame.tsx
+++ client/src/containers/MemoryGame/MemoryGame.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
 import styles from './MemoryGame.module.scss';
 import FlipsCounter from '../../components/FlipsCounter';
 import MemoryScore from '../../components/MemoryScore';
 import CardsTable from '../../components/CardsTable';
+import GameOver from '../../components/GameOver';
 import useMemoryGame, { allowedFlips } from './useMemoryGame';
 import { actions, useAppDispatch } from '../../store';
 import usePageTitle from '../../shared/usePageTitle';
-import { sleep } from '../../shared/utils';
 import { StarsCount } from '../../shared/types';
 import useMemoryGameAnimations from './useMemoryGameAnimations';
 
 const MemoryGameContainer = (): JSX.Element => {
   // states
   const dispatch = useAppDispatch();
-  const history = useHistory();
+  const [cheatUsed, setCheatUsed] = useState(false);
   const { gameState, handleCardChoice } = useMemoryGame({
     onGameOver,
   });
@@ -37,17 +36,22 @@ const MemoryGameContainer = (): JSX.Element => {
         stars: starsCount,
       })
     );
-
-    await sleep(1_000);
-    history.replace('/assign-points');
   }
+
+  function handleCheat() {
+    setCheatUsed(true);
+    onGameOver(2);
+  }
+
+  const showGameOver = gameState.isOver || cheatUsed;
 
   return (
     <div className={styles.screen}>
+      {showGameOver && <GameOver starsCount={2} showStars />}
       <MemoryScore
         starsCount={gameState.starsCount}
         numberOfMatches={gameState.matchesDone}
-        onClick={() => onGameOver(2)}
+        onClick={handleCheat}
       />
       <div className={styles.gameContent}>
         <FlipsCounter flipsLeft={allowedFlips - gameState.flipsDone} />
