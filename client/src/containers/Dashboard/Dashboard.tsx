88b1c9fff8e29b43de73f53d2a5f1583e7d6007b
Viktor <ricchiuto.viktor@gmail.com>
2021-07-06 14:21:28 +0200

    fix: title alignment in dashboard

--- client/src/containers/Dashboard/Dashboard.tsx
+++ client/src/containers/Dashboard/Dashboard.tsx
@@ -49,12 +49,12 @@ const Dashboard = (): JSX.Element => {
         <div className={styles.header}>
           <div className={styles.textContainer}>
             <div className={styles.title}>
-              <h1>Hello, </h1>
+              <h1 className={styles.pageTitle}>Hello, </h1>
               <h1 className={styles.username}>
                 {userStore.username ? userStore.username : 'coder'}
               </h1>
             </div>
-            <h2>{greetingMessage}</h2>
+            <h2 className={styles.greeting}>{greetingMessage}</h2>
           </div>
           <img
             alt="avatar logo"
