e324815366f8d78c94ef5dc0a46245e26cd30074
Viktor <ricchiuto.viktor@gmail.com>
2021-07-06 13:57:02 +0200

    feat: add animations to splash screen

--- client/src/containers/Splash/Splash.tsx
+++ client/src/containers/Splash/Splash.tsx
@@ -3,11 +3,13 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useHistory } from 'react-router-dom';
 import styles from './Splash.module.scss';
 import usePageTitle from '../../shared/usePageTitle';
+import useSplashAnimations from './useSplashAnimations';
 
 const Splash = (): JSX.Element => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   const history = useHistory();
   usePageTitle('Full Stuck');
+  useSplashAnimations();
 
   function handleButtonClick() {
     if (isAuthenticated) {
@@ -22,12 +24,12 @@ const Splash = (): JSX.Element => {
   return (
     <div className={styles.screen}>
       <div className={styles.logo}>
-        <h1>F</h1>
-        <h1>ULL</h1>
-        <h1>_</h1>
-        <h1>ST</h1>
-        <h1>U</h1>
-        <h1>CK</h1>
+        <h1 className={styles.logoPart}>F</h1>
+        <h1 className={styles.logoPart}>ULL</h1>
+        <h1 className={styles.logoPart}>_</h1>
+        <h1 className={styles.logoPart}>ST</h1>
+        <h1 className={styles.logoPart}>U</h1>
+        <h1 className={styles.logoPart}>CK</h1>
       </div>
       <p className={styles.subtitle}>
         Take part to the bootcamp, <br /> become CEO as quick as you can!
---
+++ client/src/containers/Splash/useSplashAnimations.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { slideDown, fadeIn } from '../../shared/animations';
+import splash from './Splash.module.scss';
+
+function useSplashAnimations(): void {
+  useEffect(animateItems, []);
+
+  function animateItems() {
+    gsap
+      .timeline()
+      .to(`.${splash.logo}`, {
+        y: '10em',
+      })
+      .from(`.${splash.logoPart}`, {
+        ...fadeIn,
+        y: 'random(-100, 100, 5)',
+        stagger: 0.15,
+        delay: 0.5,
+      })
+      .to(`.${splash.logo}`, {
+        y: 0,
+      })
+      .from(`.${splash.subtitle}`, slideDown, '<')
+      .from(`.${splash.button}`, slideDown, '<');
+  }
+}
+
+export default useSplashAnimations;
