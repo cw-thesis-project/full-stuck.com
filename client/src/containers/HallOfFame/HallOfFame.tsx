d87d743b56c8cbb9b2888b3561683e125db2dec4
Viktor <ricchiuto.viktor@gmail.com>
2021-07-21 08:09:07 +0200

    refactor: fix copypasta

--- client/src/containers/HallOfFame/HallOfFame.tsx
+++ client/src/containers/HallOfFame/HallOfFame.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import usePageTitle from 'shared/usePageTitle';
 import styles from './HallOfFame.module.scss';
 
-const Splash = (): JSX.Element => {
+const HallOfFame = (): JSX.Element => {
   usePageTitle('Full Stuck − Hall of Fame');
 
   return <h1 className={styles.screen}>hall of fame</h1>;
 };
 
-export default Splash;
+export default HallOfFame;
